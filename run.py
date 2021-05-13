411771ef3bf59fa41f4d25862f5649f859cc8b3f
caitaozhan <caitaozhan@gmail.com>
2021-05-13 11:25:46 -0400

    minor

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 '''
-stress a single core with matrix multiplicatio
+stress a single core with matrix multiplication
 '''
 import time
 import numpy as np
@@ -7,7 +7,7 @@ import numpy as np
 if __name__ == '__main__':
     stop = int(1e7)
     start = time.time()
-    for i in range(stop):            # time of one iteration is aroud 0.004 millisecond or 4 microseconds
+    for i in range(stop):            # time of one iteration is in the order of a few microseconds (10e-6 second)
         a = np.random.rand(10, 10)
         b = np.random.rand(10, 10)
         c = a@b

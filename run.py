454c9f16e4a1263b22294849050c27000716fa0a
Caitao Zhan <caitaozhan@Caitaos-MacBook-Pro.local>
2021-10-27 22:51:53 -0400

    update the stress.py and add datapoint for M1 Pro chip

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 '''
-stress a single core with matrix multiplication
+stress a single core with a lot of matrix multiplications
+
+10/27/2021: Note that the Apple built-in python2 and python3 are directly running on the Apple silicon,
+whereas the Anaconda's python2 and python3 has to go through the rosetta, almost doubling the runtime.
 '''
+
 import time
 import numpy as np
 
 if __name__ == '__main__':
-    stop = int(1e7)
+    repeat = int(1e7)
     start = time.time()
-    for i in range(stop):            # time of one iteration is in the order of a few microseconds (10e-6 second)
+    for i in range(repeat):
         a = np.random.rand(10, 10)
         b = np.random.rand(10, 10)
         c = np.dot(a,b)
-    print('time {:.2f}s'.format(time.time() - start))
+    print('time={:.2f}s'.format(time.time() - start))
--- stress.py
+++ stress.py
@@ -2,13 +2,41 @@
 stress the CPU
 '''
 
+import argparse
+import subprocess
+import time
 from subprocess import Popen
 
+def get_runtime(p: Popen):
+    '''get the stdout and extract the runtime
+       example stdout: b'time=2.33s\n'
+    '''
+    for line in p.stdout:
+        runtime = float(line[5:9])
+        return runtime
 
 if __name__ == '__main__':
-    task = 4
-    ps = []
-    for i in range(task):
-        command = ['python', 'run.py']
-        p = Popen(command)
+    parser = argparse.ArgumentParser(description='stress the CPU')
+    parser.add_argument('-t', '--tasks', type=int, nargs='+', default=[1, 2], help='number of tasks')
+    args = parser.parse_args()
+
+    for task in args.tasks:
+        # run the tasks
+        ps = []
+        for i in range(task):
+            command = ['python', 'run.py']
+            ps.append(Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+        
+        # wait for the tasks to finish and get each task's runtime
+        runtimes = []
+        while len(ps) > 0:
+            new_ps = []
+            for p in ps:
+                if p.poll() is None:
+                    new_ps.append(p)
+                else:
+                    runtimes.append(get_runtime(p))
+            ps = new_ps
+            time.sleep(1)
+        print(f'# of task = {task}, runtimes = {runtimes}')
 

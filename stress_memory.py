d8a2d83dace35f69b5d9155f130373680d9ba067
Caitao Zhan <caitaozhan@Caitaos-MacBook-Pro.local>
2021-10-29 10:15:03 -0400

    add memory and disk test

--- stress_memory.py
+++ stress_memory.py
@@ -1,18 +1,37 @@
 '''
-Stress the RAM by copy.deepcopy
-Stress the I/O by np.save
+Stress the RAM  by copy.deepcopy
+Stress the disk by np.save
 '''
 
 import copy
 import numpy as np
 import time
+import shutil
 
-if __name__ == '__main__':
-    # test RAM
+def test_ram():
     object = np.random.rand(1024, 1024, 128)
-    print(f'Size of object {object.nbytes / 1024**3:0.3f} GB')
+    print(f'Size of object = {object.nbytes / 1024**3:0.3f} GB')
     start = time.time()
     for _ in range(100):
         object2 = copy.deepcopy(object)
         del object2
-    print(f'Time of deep copy a 1 GB object 100 times and deleting it = {time.time() - start:0.2f}s')
+    print(f'Time of deep copying an 1 GB object 100 times and deleting it = {time.time() - start:0.2f}s')
+
+def test_disk():
+    object = np.random.rand(1024, 1024, 128)
+    print(f'Size of object = {object.nbytes / 1024**3:0.3f} GB')
+    filename = 'object.npy'
+    start = time.time()
+    for _ in range(20):
+        np.save(filename, object)
+    print(f'Time of writing an 1 GB object 20 times = {time.time() - start:0.3f}s')
+    
+    start = time.time()
+    for _ in range(20):
+        object = np.load(filename)
+    print(f'Time of reading an 1 GB object 20 times = {time.time() - start:0.3f}s')
+
+
+if __name__ == '__main__':
+    # test_ram()
+    test_disk()

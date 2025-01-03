5d3c24a071f03fc5c56b6fb652612e975d349922
Caitao Zhan <caitaozhan@gmail.com>
2025-01-03 17:52:29 -0600

    update plots

--- plot.py
+++ plot.py
@@ -10,20 +10,22 @@ def plot():
     y5 = [17,   17,   17,   18,   18,   24,   32]    # i7-13700K (8 + 8)
     y6 = [15.6, 15.9, 16.8, 19.2, 22.2, 29.0, 37.6]  # M3 Pro (6 + 5)
     y7 = [20.1, 22.1, 22.3, 22.3, 22.3, 23.6, 23.6]  # Ryzen 9 7950X3D (16)
+    y8 = [13.2, 14.0, 14.5, 14.5, 14.9, 18.5, 23.5]  # M4 Pro (10 + 4)
 
 
     plt.rcParams['font.size'] = 45
     plt.rcParams['lines.linewidth'] = 9
     fig, ax = plt.subplots(1, 1, figsize=(26, 24))
-    fig.subplots_adjust(left=0.1, right=0.98, top=0.85, bottom=0.1)
-    ax.plot(np.arange(len(y1)), y1, marker='o', markersize=20, label='i5-1038NG7, 4 (MacBook Pro, 2020)')
+    fig.subplots_adjust(left=0.1, right=0.98, top=0.845, bottom=0.1)
+    ax.plot(np.arange(len(y1)), y1, marker='o', markersize=20, label='i5-1038NG7, 4 (MBP, 2020)')
     ax.plot(np.arange(len(y2)), y2, marker='o', markersize=20, label='i7-10700KF, 8 (ABS, 2021, Ubuntu, Py3.9)')
-    ax.plot(np.arange(len(y3)), y3, marker='o', markersize=20, label='M1 Pro, 6+2 (MacBook Pro, 2021, Py3.8)')
+    ax.plot(np.arange(len(y3)), y3, marker='o', markersize=20, label='M1 Pro, 6+2 (MBP, 2021, Py3.8)')
     ax.plot(np.arange(len(y7)), y7, marker='o', markersize=20, label='Ryzen 9 7950X3D, 16 (2024, Ubuntu, Py3.12)')
-    ax.plot(np.arange(len(y4)), y4, marker='o', markersize=20, label='M2, 4+4 (MacBook Air, 2022, Py3.10)')
+    ax.plot(np.arange(len(y4)), y4, marker='o', markersize=20, label='M2, 4+4 (MBA, 2022, Py3.10)')
     ax.plot(np.arange(len(y5)), y5, marker='o', markersize=20, label='i7-13700K, 8+8 (2023, WSL2, Py3.10)')
-    ax.plot(np.arange(len(y6)), y6, marker='o', markersize=20, label='M3 Pro, 5+6 (MacBook Pro, 2023, Py3.12)')
-    fig.legend(ncol=2, fontsize=33, bbox_to_anchor=(1, 1))
+    ax.plot(np.arange(len(y6)), y6, marker='o', markersize=20, label='M3 Pro, 5+6 (MBP, 2023, Py3.12)')
+    ax.plot(np.arange(len(y8)), y8, marker='o', markersize=20, label='M4 Pro, 10+4 (MBP, 2024, Py3.12)')
+    fig.legend(ncol=2, fontsize=37, bbox_to_anchor=(1.005, 1.005))
     ax.grid()
     ax.set_xlim([-0.05, 6.05])
     ax.set_xticks(np.arange(len(X)))
